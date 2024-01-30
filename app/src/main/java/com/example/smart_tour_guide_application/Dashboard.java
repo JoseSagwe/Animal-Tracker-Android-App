95deb47d28dd10babe2795f18b74222ac90cc4c9
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-30 09:39:20 +0300

    endpoint ip address

--- app/src/main/java/com/example/smart_tour_guide_application/Dashboard.java
+++ app/src/main/java/com/example/smart_tour_guide_application/Dashboard.java
@@ -1,27 +1,27 @@
 package com.example.smart_tour_guide_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
 
-    TextView tv_first_name, tv_last_name, tv_dob, tv_location, tv_mobile, tv_email;
-    Button sign_out_btn;
+    CardView logoutCardView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        //Hook Sign Out Button
-        sign_out_btn = findViewById(R.id.sign_out_btn);
+        //Hook Sign logoutCardView
+        logoutCardView = findViewById(R.id.logoutCardView);
+
 
         //Set on click Listener
-        sign_out_btn.setOnClickListener(new View.OnClickListener() {
+        logoutCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 signUserOut();
--- app/src/main/java/com/example/smart_tour_guide_application/Registration.java
+++ app/src/main/java/com/example/smart_tour_guide_application/Registration.java
@@ -78,7 +78,7 @@ public class Registration extends AppCompatActivity {
         //Instantiate The request queue
         RequestQueue queue = Volley.newRequestQueue(Registration.this);
         //The URL Posting TO:
-        String url = "http://192.168.91.226:8080/smart/user/register";
+        String url = "http://192.168.56.1:8080/smart/user/register";
 
         //String Request Object
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
@@ -96,8 +96,8 @@ public class Registration extends AppCompatActivity {
                     confirm.setText(null);
                     Toast.makeText(Registration.this, "Registration Successful", Toast.LENGTH_LONG).show();
                 }
-
             }
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
